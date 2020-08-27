9041072cfbe39efc3450dfb9c95ee8a91a989327
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-08-26 19:17:55 -0700

    httpExamples

--- net_example/client.js
+++ net_example/client.js
@@ -1,15 +1,37 @@
 const net = require('net');
-const conn = net.createConnection({
-	host: 'localhost', // change to IP address of computer or ngrok host if tunneling
-	port: 3000, // or change to the ngrok port if tunneling
+const stdin = process.stdin;
+
+// 1 - create connection
+const client = net.createConnection({
+	host: 'tcp://0.tcp.ngrok.io', // change to IP address of computer or ngrok host if tunneling
+	port: 19488, // or change to the ngrok port if tunneling
+	// host: 'localhost',
+	// port: 3001,
 });
 
-conn.setEncoding('utf8'); // interpret data as text
+// 2 - encode the data
+client.setEncoding('utf8');
 
-conn.on('data', data => {
+// console.log(client);
+
+client.on('data', data => {
+	// 3 - getting the data from the server then log into client side
 	console.log('Server says: ', data);
 });
 
-conn.on('connect', () => {
-	conn.write('Hello from client!');
+//4 - once connected send data to the server
+client.on('connect', () => {
+	client.write('Hello from Burnaby!');
+});
+
+stdin.resume(); // activate the input
+
+stdin.setEncoding('utf8');
+
+stdin.on('data', data => {
+	client.write(data);
+});
+
+client.on('data', data => {
+	console.log('->', data);
 });
--- net_example/server.js
+++ net_example/server.js
@@ -1,16 +1,34 @@
 const net = require('net');
+
 const server = net.createServer();
 
-server.listen(3000, () => {
-	console.log('Server listening on port 3000!');
-});
+const PORT = 3001;
 
-server.on('connection', client => {
+const connections = [];
+
+server.on('connection', conn => {
+	//everytime someone connects to the server
+	// console.log(conn);
 	console.log('New client connected');
-	client.write('Hello there!');
+	conn.write('Hello there!');
+	conn.setEncoding('utf8'); //interpret data as text
+
+	// connections.push(conn);
+
+	// add the connection to our connections array
+	conn.on('data', data => {
+		console.log('Message from client: ', data);
+	});
+
+	// conn.on('data', data => {
+	// 	console.log('->', data);
+	// 	for (let connection of connections) {
+	// 		//to show everyone's data to the individual client
+	// 		connection.write(data);
+	// 	}
+	// });
 });
 
-// client.setEncoding('utf8'); //interpret data as text
-// client.on('data', data => {
-// 	console.log('Message from client: ', data);
-// });
+server.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}!`);
+});
